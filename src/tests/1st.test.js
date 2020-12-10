ebf705b556ae157824a2e2723f7075528d33c106
Dmitry Usov <usovdm@gmail.com>
2020-12-10 11:35:31 +0300

    заменяет class на function и возвращает функцию вместо объекта

--- src/tests/1st.test.js
+++ src/tests/1st.test.js
@@ -1,21 +1,32 @@
 const { By, Key, until } = require('selenium-webdriver')
 const { getDriver, afterEachCommon } = require('../utils')
 
-class Test {
+function BaseTest(fn) {
   // will be used for taking screenshots during the exceptions
   // todo: move to separate module
-  constructor(fn) {
-    this.fn = fn
+  const onTestStart = async () => { }
+
+  const onTestFinish = async () => { }
+
+  const onTestFail = async (e) => {
+    console.log(e)
   }
 
-  tryFn = async () => {
+  const baseTestRun = async () => {
     try {
-      await this.fn()
+      await onTestStart()
+      await fn()
     } catch (e) {
-      console.log(e)
+      await onTestFail(e)
       throw e
+    } finally {
+      await onTestFinish()
     }
   }
+
+  baseTestRun.origFn = fn
+
+  return baseTestRun
 }
 
 describe('1st test', () => {
@@ -26,7 +37,7 @@ describe('1st test', () => {
   afterEach(async () => {
     await afterEachCommon(driver);
   })
-  test('search name', new Test(async () => {
+  it('search name', new BaseTest(async () => {
     await driver.get("https://www.google.ru/")
     await driver.manage().window().setRect(630, 691)
     await driver.findElement(By.name("q")).click()
@@ -36,5 +47,5 @@ describe('1st test', () => {
 
     expect(text).toMatch('"name"')
     expect(text).toMatch('варианты перевода')
-  }).tryFn)
+  }))
 })
